f2322c938640411813ad7965bcfbb95fa7dd3895
deniz <deniz.aslan331@hotmail.com>
2016-06-07 18:36:33 +0300

    upload

--- sample.js
+++ sample.js
@@ -1,24 +1,3 @@
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/abc', function(request, response) {
-  response.render('pages/index');
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
-
 var readJson = require("r-json"); //can read the data from json file
 const CREDENTIALS = readJson(`${__dirname}/credentials.json`); //get the API from the json file
 // connect to the database
