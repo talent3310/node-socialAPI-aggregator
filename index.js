52d8eb821f150b6c4a76e794f71820526f657e5e
deniz <deniz.aslan331@hotmail.com>
2016-06-07 17:35:59 +0300

    upload

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/abc', function(request, response) {
   response.render('pages/index');
 });
 
