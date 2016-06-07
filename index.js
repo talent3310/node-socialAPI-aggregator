073896b6fc73106db45888e4bf074547207e4f72
deniz <deniz.aslan331@hotmail.com>
2016-06-07 18:46:41 +0300

    upload

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// // views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
-app.get('/abc', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/abc', function(request, response) {
+//   response.render('pages/index');
+// });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
 
 
 
